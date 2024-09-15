ced5b418cccb48075ba782642c8e2fb92a79066f
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-15 23:01:35 +0530

    Data Types in Javascript

---
+++ 01_FundamentalsPart-1/03_dataTypes.js
@@ -0,0 +1,18 @@
+// data types
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "ajay");
+
+javascriptIsFun = "YES";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(typeof year);
+
+year = 2000;
+console.log(year);
+
+console.log(typeof null);
