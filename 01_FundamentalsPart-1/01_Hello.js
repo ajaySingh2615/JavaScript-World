53e1a49ca44b1289ba16f26422f8047939dc78bd
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-15 22:08:19 +0530

    basis of javascript

---
+++ 01_FundamentalsPart-1/01_Hello.js
@@ -0,0 +1,4 @@
+let js = "amazing";
+if (js === "amazing") console.log("Javascript is fun");
+
+console.log(40 + 8 + 23 - 10);
---
+++ 01_FundamentalsPart-1/02_values_and_variables.js
@@ -0,0 +1,15 @@
+//values
+console.log("jonas");
+
+// declaring a variable
+let firstName = "Ajay";
+console.log(firstName);
+
+let $first = "ajay";
+console.log($first);
+
+let PI = 3.1415;
+
+let myFirstJob = "Software Developer";
+let myCurrentJob = "JavaScript Developer";
+console.log(myCurrentJob);
