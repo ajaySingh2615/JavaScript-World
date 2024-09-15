870a03531b218b55ac9176de6d5ac1432d8ca3f5
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-15 23:23:19 +0530

    let, const and var keyword

---
+++ 01_FundamentalsPart-1/04_letConstVarKeywords.js
@@ -0,0 +1,15 @@
+// let, const and var keyword
+
+let age = 30;
+age = 31;
+
+const birthYear = 2000;
+// birthYear = 2001;
+// console.log(birthYear);
+
+var job = "programmer";
+job = "teacher";
+console.log(job);
+
+lastName = "singh";
+console.log(lastName);
