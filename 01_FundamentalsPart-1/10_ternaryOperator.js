473ee7d48b1b56ced1e74f0ca784da63971cc818
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-16 02:39:06 +0530

    done fundamental part-1

---
+++ 01_FundamentalsPart-1/10_ternaryOperator.js
@@ -0,0 +1,8 @@
+const age = 18;
+
+// age >= 18
+//   ? console.log("I like to drink a wine")
+//   : console.log("I want to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
