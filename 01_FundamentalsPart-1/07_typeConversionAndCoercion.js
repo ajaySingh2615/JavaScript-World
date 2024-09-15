b5cda805411f31d62d8516be13d40b256a8ba5b0
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-16 01:16:03 +0530

    type conversion and decision making statem ets

---
+++ 01_FundamentalsPart-1/07_typeConversionAndCoercion.js
@@ -0,0 +1,16 @@
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear));
+
+console.log(Number(inputYear) + 18);
+
+console.log(typeof Number(NaN));
+
+// type coercion
+console.log("I am " + 23 + " years old");
+console.log("23" - "2");
+console.log("2" * 10);
+
+let n = "1" + 1;
+n = n - 1;
+console.log(n);
