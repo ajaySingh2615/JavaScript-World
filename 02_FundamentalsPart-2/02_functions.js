11bed44a053def37f23fac7871e18a6b7a70e15a
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-16 16:01:31 +0530

    functions in javascript

---
+++ 02_FundamentalsPart-2/02_functions.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function logger() {
+  console.log("My name is Ajay");
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
