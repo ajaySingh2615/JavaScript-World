77a05573dedfd7824a554a11b0bce0addd3f19f0
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-16 21:00:14 +0530

    Arrays done

---
+++ 02_FundamentalsPart-2/05_arrays.js
@@ -0,0 +1,16 @@
+const friends = ["mike", "eleven", "dustin", "will"];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+console.log(years);
+
+console.log(friends[0]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "jay";
+console.log(friends);
+
+const firstName = "Ajay";
+const ajay = [firstName, "singh", 2024 - 2000, "unEmployement", friends];
+console.log(ajay);
---
+++ 02_FundamentalsPart-2/06_ArraysMethods.js
@@ -0,0 +1,18 @@
+const friends = ["mike", "eleven", "dustin", "will"];
+console.log(friends);
+
+friends.push("Jay");
+console.log(friends);
+
+friends.unshift("John");
+console.log(friends);
+
+friends.pop();
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf("mike"));
+
+console.log(friends.includes("mike"));
